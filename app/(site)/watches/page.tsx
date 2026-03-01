7853f10fb81b061e155e41699b0af8e34e64a07a
fuckitoff22 <afulzele865@gmail.com>
2026-03-01 18:23:56 +0530

    Update page.tsx

--- app/(site)/watches/page.tsx
+++ app/(site)/watches/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+export const dynamic = "force-dynamic"
 import ProductGrid from "@/components/ProductGrid"
 
 export default function watchesPage() {
@@ -12,4 +12,5 @@ export default function watchesPage() {
       <ProductGrid category="watches" />
     </div>
   )
+
 }
