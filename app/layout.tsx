40433cec26c5e24d18406230a314b5ecdaddf211
fuckitoff22 <afulzele865@gmail.com>
2026-03-01 18:12:22 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
 import "./globals.css"
-import Navbar from "@/components/Navbar"
-import { CountryProvider } from "@/hooks/useCountry"
 
 export default function RootLayout({
   children,
@@ -9,12 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">
-        <CountryProvider>
-          <Navbar />
-          {children}
-        </CountryProvider>
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
