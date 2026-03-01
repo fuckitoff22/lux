5a17284dac78f890fbd7bcf1c64f84bc06c395be
fuckitoff22 <afulzele865@gmail.com>
2026-03-01 18:30:15 +0530

    Update page.tsx

--- app/(site)/clothes/page.tsx
+++ app/(site)/clothes/page.tsx
@@ -1,5 +1,5 @@
-"use client"
 export const dynamic = "force-dynamic"
+"use client"
 import ProductGrid from "@/components/ProductGrid"
 
 export default function ClothesPage() {
@@ -14,3 +14,4 @@ export default function ClothesPage() {
   )
 
 }
+
