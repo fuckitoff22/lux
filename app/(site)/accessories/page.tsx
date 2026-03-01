7f485d98a9c5a5132de5b0836b86f4a051c12451
fuckitoff22 <afulzele865@gmail.com>
2026-03-01 18:30:43 +0530

    Update page.tsx

--- app/(site)/accessories/page.tsx
+++ app/(site)/accessories/page.tsx
@@ -1,5 +1,5 @@
-"use client"
 export const dynamic = "force-dynamic"
+"use client"
 import Link from "next/link"
 import { motion } from "framer-motion"
 
@@ -81,3 +81,4 @@ export default function AccessoriesPage() {
   )
 
 }
+
