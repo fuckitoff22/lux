da111c61b65073b7d5f15c2cc25a708022e823dd
fuckitoff22 <afulzele865@gmail.com>
2026-03-05 14:10:36 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "fftxeeljgsumvalcimrq.supabase.co",
-        pathname: "/storage/v1/object/public/**",
-      },
-    ],
+    domains: ["fftxeeljgsumvalcimrq.supabase.co"],
   },
 };
 
