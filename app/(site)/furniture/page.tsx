225c480117db19ffb2c99094dc286e7ae4e3a534
fuckitoff22 <afulzele865@gmail.com>
2026-03-05 16:52:42 +0530

    Add files via upload

---
+++ app/(site)/furniture/page.tsx
@@ -0,0 +1,12 @@
+import ProductGrid from "@/components/ProductGrid"
+
+export default function furniturePage() {
+  return (
+    <div className="px-20 py-16">
+      <ProductGrid
+        category="accessories"
+        subcategory="furniture"
+      />
+    </div>
+  )
+}
