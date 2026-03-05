98b20ea63a731b12fee599a5db1ff8f796b3278d
fuckitoff22 <afulzele865@gmail.com>
2026-03-05 20:05:01 +0530

    Create not-found.tsx

---
+++ not-found.tsx
@@ -0,0 +1,9 @@
+export const dynamic = "force-dynamic"
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-white">
+      <h1 className="text-3xl font-bold">Page Not Found</h1>
+    </div>
+  )
+}
